64f773a8f18b0c5f4c31cd570201fab810af2c09
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-09-17 12:12:29 -0400

    Assets update

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,31 +1,39 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const env = process.env.MIX_ENV || "dev";
-const isProduction = env === "prod";
+const env = process.env.MIX_ENV || 'dev';
+const isProduction = env === 'prod';
 
 // We'll set up some paths for our generated files and our development server
-const staticDir = resolve(__dirname, ".");
-const destDir = resolve(__dirname, "..", "priv", "static");
-const publicPath = "/";
+const staticDir = resolve(__dirname, '.');
+const destDir = resolve(__dirname, '..', 'priv', 'static');
+const publicPath = '/';
 
 module.exports = {
-  entry: [staticDir + "/js/app.js", staticDir + "/css/app.scss"],
+  entry: [staticDir + '/js/app.js', staticDir + '/css/app.scss'],
   output: {
     path: destDir,
-    filename: "js/app.js",
+    filename: 'js/app.js',
     publicPath
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css'],
+    modules: ['node_modules'],
+    alias: {
+      jquery: resolve(__dirname, 'node_modules/jquery/dist/jquery.js'),
+      jQuery: resolve(__dirname, 'node_modules/jquery/dist/jquery.js')
+    }
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["es2015"]
+          presets: ['es2015']
         }
       },
       // Any CSS or SCSS files will run through the css loader, the sass
@@ -35,16 +43,21 @@ module.exports = {
       {
         test: /\.s?css$/,
         use: ExtractTextPlugin.extract({
-          use: "css-loader!sass-loader!import-glob-loader",
-          fallback: "style-loader"
+          use: 'css-loader!sass-loader!import-glob-loader',
+          fallback: 'style-loader'
         })
       }
     ]
   },
   // And we'll configure our ExtractTextPlugin and CopyWebpackPlugin
   plugins: [
-    new ExtractTextPlugin("css/app.css"),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new ExtractTextPlugin('css/app.css'),
     // We copy our images and fonts to the output folder
-    new CopyWebpackPlugin([{ from: "./static/images", to: "images" }])
+    new CopyWebpackPlugin([{ from: './static/images', to: 'images' }])
   ]
 };
